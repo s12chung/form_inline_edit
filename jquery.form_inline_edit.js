7fabc1a22123f2de32757a0d8df27c7cb484ae49
s12chung <steve@placemarklist.com>
2011-09-20 22:40:41 -0400

    refactor

--- jquery.form_inline_edit.js
+++ jquery.form_inline_edit.js
@@ -32,96 +32,98 @@ function d(param, def) { return typeof(param) != 'undefined' ?  param : def; }
             options = $.extend(defaults, options);
 
             var id_index = 0;
-
             return this.each(function() {
-                var $text_field = create_text_field();
-                var $self = $(this);
-                $self.after($text_field);
-
-                $text_field.finished = function() {
-                    var valid = options.valid_check($text_field.val());
-                    var empty = is_empty($text_field.val());
-                    if (valid || (empty && options.undo_empty && $self.html() != options.empty_text)) {
-                        $self.show();
-                        $text_field.hide();
-
-                        if (!(empty && options.undo_empty)) {
-                            var value = $text_field.val();
-                            if (empty)
-                                value = options.empty_text;
-                            $self.html(value);
-                            $($self.attr('href')).val($self.html());
-                        }
-                    }
-                    var final_val = $self.html().replace(options.empty_text, "");
-                    if (options.finished) options.finished(final_val, options.valid_check(final_val));
-                    return true;
-                };
-                $text_field.blur($text_field.finished);
-                if (options.return_key_finish) {
-                    $text_field.keydown(function(event){
-                        if( (event.keyCode == 13 && !event.shiftKey) ) {
-                            $text_field.blur();
-                            event.preventDefault();
-                            return false;
-                        }
-                    });
-                }
-
-                $self.show_text_field = function(focus) {
-                    focus = d(focus, true);
-                    $self.hide();
-
-                    $text_field.val($self.html().replace(options.empty_text, ""));
-                    $text_field.show();
-                    if (focus) {
-                        $text_field.focus();
-                        if (options.select_all)
-                            $text_field.select();
-                    }
-
-                    if (options.started) options.started();
-                };
-
-                var empty = is_empty($self.html());
-                if (!options.valid_check($self.html())) {
-                    $self.show_text_field();
-                }
-                if (empty)
-                    $self.html(options.empty_text);
-
-                $self.click(function() {
-                    $self.show_text_field();
-                    return false;
-                });
-
-                $self.bind("show_text_field", function(event, focus) { $self.show_text_field(focus); });
+                $.form_inline_edit($(this), options, id_index);
             });
-
-            function is_empty(value) {
-                return $.trim(value).length == 0;
-            }
-
-            function create_text_field() {
-                var $text_field;
-                if (options.textarea) {
-                    $text_field = $(document.createElement('textarea'));
-                }
-                else {
-                    $text_field = $(document.createElement('input'));
-                    $text_field.attr({ type: 'text' });
-                }
-
-                $text_field.addClass(options.class);
-                if (options.id) {
-                    $text_field.attr( { id: options.id + (id_index > 0 ? id_index : "")});
-                    id_index++;
-                }
-                $text_field.hide();
-
-                return $text_field;
-            }
         },
         show_text_field : function(focus) { return this.trigger("show_text_field", focus); }
     });
+
+    $.form_inline_edit = function($self, options, id_index) {
+        var $text_field = create_text_field();
+        $self.after($text_field);
+
+        $text_field.finished = function() {
+            var valid = options.valid_check($text_field.val());
+            var empty = is_empty($text_field.val());
+            if (valid || (empty && options.undo_empty && $self.html() != options.empty_text)) {
+                $self.show();
+                $text_field.hide();
+
+                if (!(empty && options.undo_empty)) {
+                    var value = $text_field.val();
+                    if (empty)
+                        value = options.empty_text;
+                    $self.html(value);
+                    $($self.attr('href')).val($self.html());
+                }
+            }
+            var final_val = $self.html().replace(options.empty_text, "");
+            if (options.finished) options.finished(final_val, options.valid_check(final_val));
+            return true;
+        };
+        $text_field.blur($text_field.finished);
+        if (options.return_key_finish) {
+            $text_field.keydown(function(event){
+                if( (event.keyCode == 13 && !event.shiftKey) ) {
+                    $text_field.blur();
+                    event.preventDefault();
+                    return false;
+                }
+            });
+        }
+
+        $self.show_text_field = function(focus) {
+            focus = d(focus, true);
+            $self.hide();
+
+            $text_field.val($self.html().replace(options.empty_text, ""));
+            $text_field.show();
+            if (focus) {
+                $text_field.focus();
+                if (options.select_all)
+                    $text_field.select();
+            }
+
+            if (options.started) options.started();
+        };
+
+        var empty = is_empty($self.html());
+        if (!options.valid_check($self.html())) {
+            $self.show_text_field();
+        }
+        if (empty)
+            $self.html(options.empty_text);
+
+        $self.click(function() {
+            $self.show_text_field();
+            return false;
+        });
+
+        $self.bind("show_text_field", function(event, focus) { $self.show_text_field(focus); });
+
+        function is_empty(value) {
+            return $.trim(value).length == 0;
+        }
+
+        function create_text_field() {
+            var $text_field;
+            if (options.textarea) {
+                $text_field = $(document.createElement('textarea'));
+            }
+            else {
+                $text_field = $(document.createElement('input'));
+                $text_field.attr({ type: 'text' });
+            }
+
+            $text_field.addClass(options.class);
+            if (options.id) {
+                $text_field.attr( { id: options.id + (id_index > 0 ? id_index : "")});
+                id_index++;
+            }
+            $text_field.hide();
+
+            return $text_field;
+        }
+    };
 })(jQuery);
