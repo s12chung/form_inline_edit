cf169460e1eea3bc093496e3a6ea6f8ad1e9a876
s12chung <steve@placemarklist.com>
2011-09-20 22:40:13 -0400

    added $().show_text_field()

--- jquery.form_inline_edit.js
+++ jquery.form_inline_edit.js
@@ -1,115 +1,127 @@
 /*
-MIT License
+ MIT License
 
-Copyright (c) 2011 Steven Chung
+ Copyright (c) 2011 Steven Chung
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+function d(param, def) { return typeof(param) != 'undefined' ?  param : def; }
+
 (function($){
-    $.fn.form_inline_edit = function(options) {
-        var defaults = {
-            id: null,
-            class: "form_inline_text_field",
-            textarea: false,
-            return_key_finish: true,
-            select_all: false,
-            undo_empty: true,
-            empty_text: "&nbsp;&nbsp;&nbsp;",
-            valid_check: function(value) { return true; },
-            started: null,
-            finished: null
-        };
-        //start: function() {}
-        //finished: function(value, valid) { }
-        options = $.extend(defaults, options);
+    $.fn.extend({
+        form_inline_edit: function(options) {
+            var defaults = {
+                id: null,
+                class: "form_inline_text_field",
+                textarea: false,
+                return_key_finish: true,
+                select_all: false,
+                undo_empty: true,
+                empty_text: "&nbsp;&nbsp;&nbsp;",
+                valid_check: function(value) { return true; },
+                started: null,
+                finished: null
+            };
+            //start: function() {}
+            //finished: function(value, valid) { }
+            options = $.extend(defaults, options);
 
-        var id_index = 0;
+            var id_index = 0;
 
-        return this.each(function() {
-            var $text_field = create_text_field();
-            var $self = $(this);
-            $self.after($text_field);
+            return this.each(function() {
+                var $text_field = create_text_field();
+                var $self = $(this);
+                $self.after($text_field);
 
-            $text_field.finished = function() {
-                var valid = options.valid_check($text_field.val());
-                var empty = is_empty($text_field.val());
-                if (valid || (empty && options.undo_empty && $self.html() != options.empty_text)) {
-                    $self.show();
-                    $text_field.hide();
+                $text_field.finished = function() {
+                    var valid = options.valid_check($text_field.val());
+                    var empty = is_empty($text_field.val());
+                    if (valid || (empty && options.undo_empty && $self.html() != options.empty_text)) {
+                        $self.show();
+                        $text_field.hide();
 
-                    if (!(empty && options.undo_empty)) {
-                        var value = $text_field.val();
-                        if (empty)
-                            value = options.empty_text;
-                        $self.html(value);
-                        $($self.attr('href')).val($self.html());
+                        if (!(empty && options.undo_empty)) {
+                            var value = $text_field.val();
+                            if (empty)
+                                value = options.empty_text;
+                            $self.html(value);
+                            $($self.attr('href')).val($self.html());
+                        }
                     }
+                    var final_val = $self.html().replace(options.empty_text, "");
+                    if (options.finished) options.finished(final_val, options.valid_check(final_val));
+                    return true;
+                };
+                $text_field.blur($text_field.finished);
+                if (options.return_key_finish) {
+                    $text_field.keydown(function(event){
+                        if( (event.keyCode == 13 && !event.shiftKey) ) {
+                            $text_field.blur();
+                            event.preventDefault();
+                            return false;
+                        }
+                    });
                 }
-                var final_val = $self.html().replace(options.empty_text, "");
-                if (options.finished) options.finished(final_val, options.valid_check(final_val));
-            };
-            $text_field.blur($text_field.finished);
-            if (options.return_key_finish) {
-                $text_field.keydown(function(event){
-                    if( (event.keyCode == 13 && !event.shiftKey) ) {
-                        $text_field.blur();
-                        event.preventDefault();
-                        return false;
+
+                $self.show_text_field = function(focus) {
+                    focus = d(focus, true);
+                    $self.hide();
+
+                    $text_field.val($self.html().replace(options.empty_text, ""));
+                    $text_field.show();
+                    if (focus) {
+                        $text_field.focus();
+                        if (options.select_all)
+                            $text_field.select();
                     }
+
+                    if (options.started) options.started();
+                };
+
+                var empty = is_empty($self.html());
+                if (!options.valid_check($self.html())) {
+                    $self.show_text_field();
+                }
+                if (empty)
+                    $self.html(options.empty_text);
+
+                $self.click(function() {
+                    $self.show_text_field();
+                    return false;
                 });
-            }
 
-            $self.show_text_field = function() {
-                $self.hide();
-
-                $text_field.val($self.html().replace(options.empty_text, ""));
-                $text_field.show();
-                $text_field.focus();
-                if (options.select_all)
-                    $text_field.select();
-
-                if (options.started) options.started();
-            };
-            var empty = is_empty($self.html());
-            if (!options.valid_check($self.html())) {
-                $self.show_text_field();
-            }
-            if (empty)
-                $self.html(options.empty_text);
-
-            $self.click(function() {
-                $self.show_text_field();
-                return false;
+                $self.bind("show_text_field", function(event, focus) { $self.show_text_field(focus); });
             });
-        });
 
-        function is_empty(value) {
-            return $.trim(value).length == 0;
-        }
-
-        function create_text_field() {
-            var $text_field;
-            if (options.textarea) {
-                $text_field = $(document.createElement('textarea'));
-            }
-            else {
-                $text_field = $(document.createElement('input'));
-                $text_field.attr({ type: 'text' });
+            function is_empty(value) {
+                return $.trim(value).length == 0;
             }
 
-            $text_field.addClass(options.class);
-            if (options.id) {
-                $text_field.attr( { id: options.id + (id_index > 0 ? id_index : "")});
-                id_index++;
-            }
-            $text_field.hide();
+            function create_text_field() {
+                var $text_field;
+                if (options.textarea) {
+                    $text_field = $(document.createElement('textarea'));
+                }
+                else {
+                    $text_field = $(document.createElement('input'));
+                    $text_field.attr({ type: 'text' });
+                }
 
-            return $text_field;
-        }
-    };
+                $text_field.addClass(options.class);
+                if (options.id) {
+                    $text_field.attr( { id: options.id + (id_index > 0 ? id_index : "")});
+                    id_index++;
+                }
+                $text_field.hide();
+
+                return $text_field;
+            }
+        },
+        show_text_field : function(focus) { return this.trigger("show_text_field", focus); }
+    });
 })(jQuery);
